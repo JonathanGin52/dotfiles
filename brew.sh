b0214effa53f748e9473d1fa154b273c6d20c7b7
Jonathan Gin <jonathan.gin@shopify.com>
2019-02-02 00:00:36 -0500

    Fixed broken heroku install

--- brew.sh
+++ brew.sh
@@ -19,7 +19,7 @@ brew install node # This also installs `npm` using the recommended installation 
 brew install bat
 brew install tree
 brew install vim --with-override-system-vi
-brew install heroku
+brew install heroku/brew/heroku
 
 # Install Python
 brew install python
