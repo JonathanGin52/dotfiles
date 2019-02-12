861b909f2622bf6f877e215096ea78453c57ba20
Jonathan Gin <jonathangin52@gmail.com>
2019-02-12 11:28:47 -0500

    Moved homebrew check to setup.sh

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Check for Homebrew,
-# Install if we don't have it
-if test ! $(which brew); then
-  echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
 # Use the latest version of Homebrew
 brew update
 
--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until `zsh.sh` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
-# Check for Homebrew, install if we don't have it
-if test ! $(which brew); then
-  echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-brew install zsh
+# Install zsh
+brew info zsh | grep --quiet 'Not installed' && brew install zsh
 
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
