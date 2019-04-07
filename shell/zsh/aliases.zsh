648b33eebc64adbe58488eea0430ae0bdc22e6d3
Jonathan Gin <jonathangin52@gmail.com>
2019-04-07 12:44:02 -0400

    Change update_all alias to include brew cask

--- shell/zsh/aliases.zsh
+++ shell/zsh/aliases.zsh
@@ -13,6 +13,7 @@ alias pip3up='pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -
 alias update_all="sudo softwareupdate --install --all \
   && brew update \
   && brew upgrade \
+  && brew cask upgrade --greedy \
   && brew cleanup \
   && pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U \
   && npm install -g npm \
