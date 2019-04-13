b37c6ef0e8318654b698b3938c18acc5ea590a96
Jonathan Gin <jonathangin52@gmail.com>
2019-04-13 12:09:04 -0400

    Remove rails plugin

--- shell/zsh/aliases.zsh
+++ shell/zsh/aliases.zsh
@@ -5,8 +5,15 @@ alias tls='tmux list-sessions'
 alias tksv='tmux kill-server'
 alias tkss='tmux kill-session -t'
 
+# Rails aliases
+alias rdm="rake db:migrate"
+alias rdms="rake db:migrate:status"
+alias rdr="rake db:rollback"
+alias rrg="rake routes | grep"
+alias rgm="rails generate migration"
+
 alias ssh="TERM=xterm-256color ssh"
-alias hgrep='history | grep --color=auto'
+alias hgrep='history | rg --color=auto'
 alias cleanupds='find . -type f -name "*.DS_Store" -ls -delete'
 alias pipup='pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U'
 alias pip3up='pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U'
--- shell/zsh/plugins.zsh
+++ shell/zsh/plugins.zsh
@@ -1,7 +1,6 @@
 plugins=(
   zsh-autosuggestions
   zsh-syntax-highlighting
-  rails
   osx
   web-search
   z
