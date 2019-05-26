53b028d6d73870e360fe9f4444bfbf910b27b37c
Jonathan Gin <jonathangin52@gmail.com>
2019-05-26 17:30:19 -0400

    Switch shell to alacritty

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -3,8 +3,8 @@
 # Install zsh
 brew info zsh | grep --quiet 'Not installed' && brew install zsh
 
-# Install iterm2
-brew cask info iterm2 | grep --quiet 'Not installed' && brew cask install iterm2
+# Install alacritty
+brew cask info alacritty | grep --quiet 'Not installed' && brew cask install alacritty
 
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -15,21 +15,21 @@ git -C ~/.oh-my-zsh/custom/plugins clone https://github.com/zsh-users/zsh-syntax
 # Install zsh autosuggestions
 git -C ~/.oh-my-zsh/custom/plugins clone https://github.com/zsh-users/zsh-autosuggestions
 
-# Install powerlevel9k theme
-git -C ~/.oh-my-zsh/custom/themes clone https://github.com/bhilburn/powerlevel9k.git
+# Install pure prompt
+npm install --global pure-prompt
 
-# Install fonts
-cp "Source Code Pro for Powerline.otf" ~/Library/Fonts
+# Add italics support in terminal and tmux
+tic -x shell/xterm-256color-italic.terminfo
+tic -x shell/tmux-256color-italic.terminfo
 
-# Install italics support in terminal and tmux
-tic -x xterm-256color-italic.terminfo
-tic -x tmux-256color-italic.terminfo
+# Symlink alacritty config
+mkdir -p ~/.config/alacritty
+ln -sf shell/alacritty.yml ~/.config/alacritty/alacritty.yml
 
-# Copy iterm2 settings
-cp com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
+# Install fzf command tools
+/usr/local/opt/fzf/install
 
 # Change default shell to zsh
 zsh_path=$(which zsh)
 grep -Fxq "$zsh_path" /etc/shells || sudo bash -c "echo $zsh_path >> /etc/shells"
 chsh -s "$zsh_path" $USER
-
