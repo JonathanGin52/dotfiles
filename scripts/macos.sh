17f6744393fa095aae4a02b66cbedfa9cf20ace5
Jonathan Gin <jonathangin52@gmail.com>
2019-06-07 19:43:06 -0400

    update macos.sh

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -44,12 +44,6 @@ sudo pmset -a hibernatemode 0
 ###############################################################################
 # Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
 ###############################################################################
-
-# Trackpad: enable tap to click for this user and for the login screen
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-
 # Disable “natural” (Lion-style) scrolling
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool true
 
