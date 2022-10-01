b3fc3cba14ae79fe92b2a404a500a4d7fa05584d
Jonathan Gin <jonathangin52@github.com>
2022-10-01 01:20:30 -0400

    Re-org null-ls

--- home/.config/nvim/lua/lsp.lua
+++ home/.config/nvim/lua/lsp.lua
@@ -1,4 +1,19 @@
 local nvim_lsp = require("lspconfig")
+local null_ls = require("null-ls")
+
+null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.erb_lint,
+    null_ls.builtins.formatting.rubocop,
+
+    null_ls.builtins.diagnostics.eslint,
+
+    null_ls.builtins.completion.spell,
+
+    null_ls.builtins.code_actions.gitsigns,
+  },
+})
+
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
@@ -33,7 +48,7 @@ end
 -- Use a loop to conveniently both setup defined servers 
 -- and map buffer local keybindings when the language server attaches
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-local servers = { "solargraph", "sorbet", "tsserver", "gopls", "vimls", "pyright" }
+local servers = { "null-ls", "solargraph", "sorbet", "tsserver", "gopls", "vimls", "pyright" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     capabilities = capabilities,
