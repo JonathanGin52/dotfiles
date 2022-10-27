81fe986e6eff08efcc5eecfd4cca703cf02cc242
Jonathan Gin <jonathangin52@github.com>
2022-10-27 11:10:46 -0400

    Remove solargraph

--- nvim/config/nvim/lua/config/lsp.lua
+++ nvim/config/nvim/lua/config/lsp.lua
@@ -52,7 +52,7 @@ end
 -- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-local servers = { "null-ls", "solargraph", "sorbet", "tsserver", "gopls", "vimls", "pyright", "bashls" }
+local servers = { "null-ls", "sorbet", "tsserver", "gopls", "vimls", "pyright", "bashls" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup({
     capabilities = capabilities,
