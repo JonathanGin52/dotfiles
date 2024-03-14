264efa6349aa605b1b780799c6418bd91fb89886
Jonathan Gin <jonathangin52@gmail.com>
2024-03-14 12:50:42 -0400

    Remove unused lsp servers

--- nvim/config/nvim/lua/config/lsp.lua
+++ nvim/config/nvim/lua/config/lsp.lua
@@ -43,7 +43,7 @@ end
 -- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-local servers = { "sorbet", "tsserver", "gopls", "vimls", "pyright", "bashls" }
+local servers = { "sorbet", "tsserver", "gopls", "vimls", "pyright" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup({
     capabilities = capabilities,
