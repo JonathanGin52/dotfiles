cfac1aec342586d4aaae815a31a3d7c18955eaa2
Jonathan Gin <jonathangin52@gmail.com>
2024-04-15 18:41:40 -0400

    Update conform config

--- nvim/config/nvim/lua/config/conform.lua
+++ nvim/config/nvim/lua/config/conform.lua
@@ -19,8 +19,61 @@ conform.setup({
     lua = { "stylua" },
     python = { "isort", "black" },
   },
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+})
+
+local slow_format_filetypes = {}
+conform.setup({
+  format_on_save = function(bufnr)
+    -- Disable with a global or buffer-local variable
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+
+    if slow_format_filetypes[vim.bo[bufnr].filetype] then
+      return
+    end
+    local function on_format(err)
+      if err and err:match("timeout$") then
+        slow_format_filetypes[vim.bo[bufnr].filetype] = true
+      end
+    end
+
+    return { timeout_ms = 200, lsp_fallback = true }, on_format
+  end,
+
+  format_after_save = function(bufnr)
+    if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+      return
+    end
+    return { lsp_fallback = true }
+  end,
+})
+
+vim.api.nvim_create_user_command("Format", function(args)
+  local range = nil
+  if args.count ~= -1 then
+    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+    range = {
+      start = { args.line1, 0 },
+      ["end"] = { args.line2, end_line:len() },
+    }
+  end
+  conform.format({ async = true, lsp_fallback = true, range = range })
+end, { range = true })
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save for this buffer",
+  bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
 })
